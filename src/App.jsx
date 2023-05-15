41cf1fcbb7624d515567bdebf05172fd023a6925
Siddhant Misra <smisra@inhance.com>
2023-05-14 17:41:36 -0700

    toast and layouts and components structure added

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,38 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-// Routes
+import Main, { mainLoader } from "./layouts/Main";
+import { logoutAction } from "./actions/logout";
+import { ToastContainer } from 'react-toastify';
+
 import Dashboard, { dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
-    loader: dashboardLoader,
-    errorElement: <Error />
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />
+      },
+      {
+        path: "logout",
+        action: logoutAction
+      }
+    ]
   },
 ]);
 
 function App() {
   return <div className="App">
     <RouterProvider router={router} />
+    <ToastContainer />
   </div>;
 }
 
